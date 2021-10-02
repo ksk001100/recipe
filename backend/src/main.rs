b9fa1382f39ae696b1af9c6997b84af92ebe424d
Keisuke Toyota <hm.pudding0715@gmail.com>
2021-10-03 03:39:30 +0900

    first commit

---
+++ backend/src/main.rs
@@ -0,0 +1,26 @@
+use axum::{handler::get, response::Json, Router};
+use serde::{Serialize, Deserialize};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/api", get(handler));
+
+    // run it
+    let addr = SocketAddr::from(([0, 0, 0, 0], 5000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Serialize)]
+struct User {
+    name: String,
+}
+
+async fn handler() -> Json<User> {
+    Json(User { name: "Keisuke Toyota".to_string() })
+}
---
+++ frontend/src/Main.elm
@@ -0,0 +1,110 @@
+module Main exposing (..)
+
+-- import Html exposing (Html, audio, button, div, h1, img, text)
+-- import Html.Attributes exposing (id, src, style)
+-- import Html.Events exposing (onClick)
+
+import Browser
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes exposing (src)
+import Http
+import Json.Decode exposing (Decoder, field, string)
+
+
+
+---- MODEL ----
+
+
+type alias User =
+    { name : String
+    }
+
+
+type alias Model =
+    { name : Maybe String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { name = Nothing }, getUser )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = GotUser (Result Http.Error User)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotUser result ->
+            case result of
+                Ok user ->
+                    ( { model | name = Just user.name }, Cmd.none )
+
+                Err _ ->
+                    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ img [ src "/logo.svg" ] []
+        , h1 []
+            [ text
+                ("My name is "
+                    ++ (case model.name of
+                            Just name ->
+                                name
+
+                            Nothing ->
+                                "..."
+                       )
+                )
+            ]
+        ]
+
+
+
+---- HTTP ----
+
+
+getUser : Cmd Msg
+getUser =
+    Http.request
+        { method = "GET"
+        , headers = []
+        , url = "http://localhost/api"
+        , expect = Http.expectJson GotUser userDecoder
+        , body = Http.emptyBody
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+userDecoder : Decoder User
+userDecoder =
+    Json.Decode.map User
+        (Json.Decode.field "name" Json.Decode.string)
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
